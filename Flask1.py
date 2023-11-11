acd65c0cb7990458a11fe2090f1060f0d7be8bf2
Navneeth <navneethsanthosh03@gmail.com>
2023-11-11 13:18:51 +0530

    commit3

--- Flask1.py
+++ Flask1.py
@@ -7,11 +7,42 @@ from io import BytesIO
 import base64
 import matplotlib.pyplot as plt
 import time
+from itertools import groupby
 
 app = Flask(__name__)
 app.secret_key = 'your_secret_key'
 
 class MoneyManager:
+
+    def update_budget(self, username, new_budget):
+        budget_data = self.load_budget_data()
+        if username in budget_data:
+            budget_data[username]['budget'] = new_budget
+            self.save_budget_data(budget_data)
+        else:
+            budget_data[username] = {'budget': 0}
+            self.save_budget_data(budget_data)
+
+    def view_budget(self, username):
+        budget_data = self.load_budget_data()
+        if username in budget_data:
+            return budget_data[username].get('budget')
+        else:
+            return 0
+        
+    def load_budget_data(self):
+        budget_file = 'budgets.json'
+        if os.path.exists(budget_file):
+            with open(budget_file, 'r') as file:
+                return json.load(file)
+        else:
+            return {}
+        
+    def save_budget_data(self, budget_data):
+        budget_file = 'budgets.json'
+        with open(budget_file, 'w') as file:
+            json.dump(budget_data, file)
+        
     def __init__(self, filename='transactions2.json'):
         self.filename = filename
         self.transactions = self.load_transactions()
@@ -69,29 +100,20 @@ class MoneyManager:
             json.dump({}, file)
 
     def register_user(self, username, password):
-        # Load existing user data from the file
         if os.path.exists("user_data.json"):
             with open("user_data.json", "r") as file:
                 user_data = json.load(file)
         else:
             user_data = []
-
-        # Check if the username already exists
         for user_entry in user_data:
             if user_entry['username'] == username:
                 flash('Username already exists. Please choose a different one.', 'error')
                 return
-
-        # Add a new user entry to the user_data list
         new_user_entry = {'username': username, 'password': password}
         user_data.append(new_user_entry)
-
-        # Write the updated user_data back to the file
         with open("user_data.json", "w") as file:
             json.dump(user_data, file)
-
         flash('Registration successful! You can now log in.', 'success')
-
 
     def validate_login(self, entered_username, entered_password):
         with open("user_data.json", "r") as file:
@@ -106,19 +128,22 @@ class MoneyManager:
     
 manager = MoneyManager()
 
-def create_expenses_chart(balance, budget,username):
-    fig, ax = Figure(), FigureCanvas(Figure())
-    ax = fig.add_subplot(111)
+def create_expenses_chart(balance, budget, username):
+    fig, ax = plt.subplots(figsize=(6, 6), facecolor='#010408')  # Set the size and background color
     transactions = get_user_transactions(username)
-    descriptions = [transaction['description'] for transaction in transactions if transaction['type']=='debit']
-    amounts = [transaction['amount'] for transaction in transactions if transaction['type']=='debit']      
-    ax.pie(amounts, labels=descriptions, autopct='%1.1f%%', startangle=90)
+    descriptions = [transaction['description'] for transaction in transactions if transaction['type'] == 'debit']
+    amounts = [transaction['amount'] for transaction in transactions if transaction['type'] == 'debit']    
+    explode = (0.1,) * len(amounts)  # Explode all slices slightly for emphasis    
+    colors = plt.cm.Paired(range(len(descriptions)))  # Use a color map for variety    
+    ax.pie(amounts, labels=descriptions, autopct='%1.1f%%', startangle=90, explode=explode, colors=colors, labeldistance=1.05, textprops={'color': 'white'})
+    centre_circle = plt.Circle((0, 0), 0.70, fc='none')
+    ax.add_patch(centre_circle)    
     ax.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
-    ax.set_title('Expenses Overview')
+    ax.set_title('Expenses Overview', fontsize=16, color='#ffffff')  # Set title color to white
     image_stream = BytesIO()
-    fig.savefig(image_stream, format='png')
+    fig.savefig(image_stream, format='png', bbox_inches='tight', transparent=True)  # Set transparent background
     image_stream.seek(0)
-    encoded_image = base64.b64encode(image_stream.read()).decode('utf-8')       
+    encoded_image = base64.b64encode(image_stream.read()).decode('utf-8')
     return encoded_image
 
 def create_income_chart(balance, budget,username):
@@ -129,50 +154,92 @@ def create_income_chart(balance, budget,username):
     amounts = [transaction['amount'] for transaction in transactions if transaction['type']=='credit']      
     ax.pie(amounts, labels=descriptions, autopct='%1.1f%%', startangle=90)
     ax.axis('equal')
-    ax.set_title('Expenses Overview')
+    ax.set_title('Income Overview')
     image_stream = BytesIO()
     fig.savefig(image_stream, format='png')
     image_stream.seek(0)
     encoded_image = base64.b64encode(image_stream.read()).decode('utf-8')       
     return encoded_image
+
+def create_budget_chart(balance, budget, username):
+
+    fig, ax = plt.subplots(figsize=(6, 6), facecolor='#010408')  # Set the size and background color
+    transactions = get_user_transactions(username)
+    expenses = [transaction["amount"] for transaction in transactions if transaction.get("type") == "debit"]
+    colors = ['#ff9999', '#66b3ff']
+    explode = (0.1, 0)
+    if budget-sum(expenses)>0:
+        ax.pie([sum(expenses), budget - sum(expenses)], labels=["Spent", "Remaining"],
+            autopct='%1.1f%%', startangle=90, colors=colors, explode=explode,
+            wedgeprops=dict(width=0.4, edgecolor='black'),textprops={'color': 'white','fontsize':14,'fontweight':'bold'})
+    else:
+        ax.pie([sum(expenses), 0], labels=["Spent", "Remaining"],
+            autopct='%1.1f%%', startangle=90, colors=colors, explode=explode,
+            wedgeprops=dict(width=0.4, edgecolor='black'),textprops={'color': 'white','fontsize':14,'fontweight':'bold'})
+        ax.text(0, 0, "Over Budget!", ha='center', va='center', fontsize=20, color='red')
+    centre_circle = plt.Circle((0, 0), 0.50, fc='none')
+    ax.add_patch(centre_circle)
+    ax.axis('equal')
+    ax.set_title('Budget Overview', fontsize=16, color='#ffffff')
+
+    image_stream = BytesIO()
+    fig.savefig(image_stream, format='png', bbox_inches='tight', transparent=True)  # Set transparent background
+    image_stream.seek(0)
+    encoded_image = base64.b64encode(image_stream.read()).decode('utf-8')
+    return encoded_image
+
 
 def get_user_transactions(username):
     with open('transactions2.json', 'r') as file:
         data = json.load(file)
         return data.get(username,[])
 
-def create_overview_plots(balance,budget,username):
+def create_overview_plots(balance, budget, username):
     transactions = get_user_transactions(username)
     # Extracting expense, budget, and income data
-    expenses = [transaction["amount"] for transaction in transactions if transaction.get("type") == "debit"]
-    budget = 200000  # Replace with your actual budget value
+    expenses = [transaction["amount"] for transaction in transactions if transaction.get("type") == "debit"]  # Replace with your actual budget value
     income = [transaction["amount"] for transaction in transactions if transaction.get("type") == "credit"]
 
     # Create subplots
-    fig, axs = plt.subplots(1, 3, figsize=(15, 5))
+    fig, axs = plt.subplots(3, 1, figsize=(10, 12), facecolor='none')  # Set the size and background color
+    for ax in axs:
+        for text in ax.get_xticklabels() + ax.get_yticklabels():
+            text.set_color('#ffffff')
 
-    # Plot Expense Overview
+    # Plot Budget Overview
     axs[0].pie([sum(expenses), budget - sum(expenses)], labels=["Spent", "Remaining"], autopct='%1.1f%%', startangle=90, colors=['red', 'green'])
     axs[0].axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
-    axs[0].set_title('Budget Overview')
-    
+    axs[0].set_title('Budget Overview', fontsize=16, color='#ffffff')
+
     # Plot Expenses Overview
-    descriptions1 = [transaction['description'] for transaction in transactions if transaction['type']=='debit']
-    amounts1 = [transaction['amount'] for transaction in transactions if transaction['type']=='debit']
-    axs[1].bar(descriptions1, amounts1)
-    axs[1].set_title('Expenses Overview')
+    descriptions1 = [transaction['description'] for transaction in transactions if transaction['type'] == 'debit']
+    amounts1 = [transaction['amount'] for transaction in transactions if transaction['type'] == 'debit']
+    axs[1].bar(descriptions1, amounts1, color='blue')  # Adjust color if needed
+    axs[1].set_title('Expenses Overview', fontsize=16, color='#ffffff')
 
     # Plot Income Overview
-    descriptions = [transaction['description'] for transaction in transactions if transaction['type']=='credit']
-    amounts = [transaction['amount'] for transaction in transactions if transaction['type']=='credit']
-    axs[2].bar(descriptions, amounts)
-    axs[2].set_title('Income Overview')
+    descriptions = [transaction['description'] for transaction in transactions if transaction['type'] == 'credit']
+    amounts = [transaction['amount'] for transaction in transactions if transaction['type'] == 'credit']
+    axs[2].bar(descriptions, amounts, color='green')  # Adjust color if needed
+    axs[2].set_title('Income Overview', fontsize=16, color='#ffffff')
+
+    for ax in axs:
+        ax.title.set_color('#ffffff')  # Set title color to white
+        ax.spines['bottom'].set_color('#ffffff')  # Set x-axis color to white
+        ax.spines['top'].set_color('#ffffff')
+        ax.spines['right'].set_color('#ffffff')
+        ax.spines['left'].set_color('#ffffff')
+        ax.yaxis.label.set_color('#ffffff')  # Set y-axis label color to white
+        ax.xaxis.label.set_color('#ffffff')
+
+    plt.tight_layout(pad=3)  # Adjust the spacing between subplots
 
     image_stream = BytesIO()
-    fig.savefig(image_stream, format='png')
+    fig.savefig(image_stream, format='png', facecolor=fig.get_facecolor(), transparent=True)  # Set transparent background
     image_stream.seek(0)
-    encoded_image = base64.b64encode(image_stream.read()).decode('utf-8')       
+    encoded_image = base64.b64encode(image_stream.read()).decode('utf-8')
     return encoded_image
+
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
@@ -188,6 +255,18 @@ def register():
             flash('Please enter both username and password.', 'error')
 
     return render_template('register.html')
+
+@app.route('/update_budget', methods=['POST'])
+def update_budget():
+    if 'username' not in session:
+        flash('Please log in first', 'error')
+        return redirect(url_for('login'))
+
+    if request.method == 'POST':
+        new_budget = float(request.form['monthly-budget'])
+        manager.update_budget(session['username'], new_budget)
+
+    return redirect(url_for('dashboard'))
 
 @app.route('/visualization')
 def visualization():
@@ -238,16 +317,23 @@ def index():
     else:
         return redirect(url_for('login'))
 
-@app.route('/dashboard')
+@app.route('/dashboard', methods=['GET', 'POST'])
 def dashboard():
-    budget = 1000
+    budget = manager.view_budget(session['username'])   
     if 'username' not in session:
         flash('Please log in first', 'error')
         return redirect(url_for('login'))
     
+    if request.method == 'POST':
+        if 'update-budget' in request.form:
+            new_budget = float(request.form['monthly-budget'])
+            manager.update_budget(session['username'], new_budget)
+            budget = manager.view_budget(session['username'])
+
     balance = manager.view_balance(session['username'])
-    chart_image = create_overview_plots(balance, budget,session['username'])
-    return render_template('dashboard.html', balance=balance, chart_image=chart_image)
+    chart_image = create_budget_chart(balance, budget, session['username'])
+    return render_template('dashboard.html', balance=balance, chart_image=chart_image, budget=budget)
+
 
 @app.route('/add_transaction', methods=['GET', 'POST'])
 def add_transaction():
@@ -289,7 +375,11 @@ def view_transactions():
         return redirect(url_for('login'))
 
     transactions = manager.view_transactions(session['username'])
-    return render_template('view_transactions.html', transactions=transactions)
+    transactions_by_category = {}
+    transactions.sort(key=lambda x: x['category'])
+    for category, group in groupby(transactions, key=lambda x: x['category']):
+        transactions_by_category[category] = list(group)
+    return render_template('view_transactions.html', transactions_by_category=transactions_by_category)
 
 if __name__ == '__main__':
     app.run(debug=True)
